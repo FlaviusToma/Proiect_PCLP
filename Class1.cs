c0630addd43aed26394824c3479f8f56769f26ac
FlaviusToma <flavius-ionut.toma@student.unitbv.ro>
2024-04-16 18:42:32 +0300

    init

---
+++ Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magazin
+{
+    class Client
+    {
+        private string idClient;
+        private string nume;
+        private string telefon;
+        private string email;
+        public Client()
+        {
+
+        }
+    }
+}
